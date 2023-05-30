f151f20d8a91fec42768f658004b164634b4e742
YASH SINHA <74976948+yashsinha0206@users.noreply.github.com>
2023-05-30 20:21:09 +0530

    Add files via upload

---
+++ TwoSum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> r;
+        int s = nums.size();
+        int diff;
+        map<int, int> m;
+        for(int i=0;i<s;i++)
+        {
+            diff = target - nums[i];
+            if(m.find(diff) != m.end() && m.find(diff)->second != i)
+            {
+                r.push_back(i);
+                r.push_back(m.find(diff)->second);
+                return r;
+            }
+            m[nums[i]] = i;
+        }
+        return r;
+    }
+};
